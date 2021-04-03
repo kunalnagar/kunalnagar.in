0b603ff46e6a7b9668e19e9540b97630011a6139
Kunal Nagar <knlnagar@gmail.com>
2021-04-03 11:12:38 -0400

    Initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,9 +22,37 @@ function enablePostFiltering() {
   document.getElementById('search').addEventListener('input', filterPosts);
 }
 
+function handleColorScheme() {
+  var $html = document.getElementsByTagName('html')[0];
+  var $btnToggleColorScheme = document.getElementById('btn_color_scheme');
+  if (localStorage.getItem('COLOR_SCHEME') === null) {
+    $html.classList.add('light');
+    localStorage.setItem('COLOR_SCHEME', 'light');
+    $btnToggleColorScheme.textContent = 'Switch to Dark Mode';
+  } else if (localStorage.getItem('COLOR_SCHEME') === 'light') {
+    $html.classList.add('light');
+    $btnToggleColorScheme.textContent = 'Switch to Dark Mode';
+  } else {
+    $html.classList.add('dark');
+    $btnToggleColorScheme.textContent = 'Switch to Light Mode';
+  }
+  $btnToggleColorScheme.addEventListener('click', function () {
+    if ($html.classList.contains('light')) {
+      $html.classList.replace('light', 'dark');
+      $btnToggleColorScheme.textContent = 'Switch to Light Mode';
+      localStorage.setItem('COLOR_SCHEME', 'dark');
+    } else {
+      $html.classList.replace('dark', 'light');
+      $btnToggleColorScheme.textContent = 'Switch to Dark Mode';
+      localStorage.setItem('COLOR_SCHEME', 'light');
+    }
+  });
+}
+
 ready(function () {
   var $blogListPage = document.querySelector('.page-blog');
   if ($blogListPage) {
     enablePostFiltering();
   }
+  handleColorScheme();
 });
