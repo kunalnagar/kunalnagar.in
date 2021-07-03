28d91d7123d65b233d2575f18efc5b6674a815d8
Kunal Nagar <knlnagar@gmail.com>
2021-07-03 16:25:51 -0400

    Update assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,13 @@ module.exports = {
   ci: {
     collect: {
       assert: {
-        preset: 'lighthouse:recommended',
+        assertions: {
+          'categories:performance': ['warn', { minScore: 0.9 }],
+          'categories:accessibility': ['error', { minScore: 0.9 }],
+          'categories:accessibility': ['error', { minScore: 0.9 }],
+          'categories:seo': ['error', { minScore: 0.9 }],
+          'categories:best-practices': ['error', { minScore: 0.9 }],
+        },
       },
       url: [
         'http://localhost:4000/',
